386be918c7427ab0d72c0bd4888f93fff1d3186e
emotion <a0968683953@gmail.com>
2021-06-15 15:19:41 +0800

    chess0.4 lobby完成

--- class/database.php
+++ class/database.php
@@ -4,7 +4,7 @@
         private $UserName = "emotion";
         private $password = "123456";
         private $DBName = "chess";
-        private $conn;
+        public $conn;
         function connect(){
             $this->conn = mysqli_connect($this->ServerName,$this->UserName,$this->password,$this->DBName);
         }
@@ -18,7 +18,6 @@
                 echo "使用者名稱已被使用";
             }
         }
-
         function signIn($id,$password){
             $sql = "SELECT id,password FROM users WHERE id = '$id'";
             $result = mysqli_query($this->conn,$sql);
@@ -35,7 +34,6 @@
                 echo "帳號或密碼不正確";
             } 
         }
-
         function get_WinStatistical($id){
             $winStatistical = new SplFixedArray(3);
             $sql = "SELECT win,loss,tie FROM users WHERE id = '$id'";
@@ -45,6 +43,29 @@
             $winStatistical[1] = $row["loss"];
             $winStatistical[2] = $row["tie"];
             return $winStatistical;
+        }
+        function get_table($table_id){
+            $sql="SELECT*FROM lobby where table_id = $table_id";
+            $result = mysqli_query($this->conn,$sql);
+            if($row = mysqli_fetch_assoc($result)){
+                $_SESSION["table_id"] = $row["table_id"];
+                $_SESSION["time"] = $row["time"];
+                $_SESSION["player1"] = $row["player1"];
+                $_SESSION["player2"] = $row["player2"];
+                $_SESSION["status"] = $row["status"];
+            }
+        }
+        function add_newtable(){
+            for($i = 1;$i <= 100;$i++){
+                $sql="SELECT*FROM lobby where table_id = $i";
+                $result = mysqli_query($this->conn,$sql);
+                if(mysqli_num_rows($result) == 0){
+                    $sql = "INSERT INTO lobby(table_id,time) VALUES($i,now())";
+                    mysqli_query($this->conn,$sql);
+                    header("location:game.php");
+                    exit;
+                }
+            }
         }
     }
 ?>
--- lobby.php
+++ lobby.php
@@ -1,4 +1,14 @@
 <html>
+
+<?php
+    include "class/database.php";
+    $database = new database();
+    $database->connect();
+    if(isset($_POST["create"])){
+        $database->add_newtable();                   
+    }
+    session_start();
+?>
     <head>
         <title>西洋棋-大廳</title>
         <style type ="text/css">
@@ -7,7 +17,7 @@
         .center{width:50%;height:100%;margin:0px;background-color:#FFFFFF;display:inline-block;text-align: center;vertical-align:top}
         .right{width:24%;height:100%;margin:0px;background-color:#FFF8D7;display:inline-block;vertical-align:top}
         .table{background-color:#FFFFFF;border-color:#0066CC;line-height:30px}
-        .table-button{background-color:#ACD6FF;line-height:20px;border:5px #0066CC solid}
+        .table-button{background-color:#ACD6FF;line-height:15px;border:5px #0066CC solid}
         .table-table{font-size:20px;font-weight:bold}
         .button{height:50px;margin-top:450px;text-align: center}
         .newtable-button{width:250px;height:40px;font-size:20px}
@@ -18,36 +28,51 @@
         <div class = "container">
             <div class = "left"></div>
             <div class = "center">
-                <form class = "table">
-                    <br/>
-                    <button type="submit" class = "table-button">
-                        <br/>
-                        <table width = "450px" hight = "300px" cellpading = "1000" class = "table-table">
-                            <tr>
-                                <td rowspan = "2" style = "color:#0066CC">06/10 3:14</td>
-                                <td style = "color:#FFFFFF">白方</td>
-                                <td>黑方</td>
-                                <td rowspan = "2" style = "color:#0066CC">遊玩中</td>
-                            </tr>
-                            <tr>
-                                <td style = "color:#FFFFFF"><br/>emotion</td>
-                                <td><br/>sickness</td>
-                            </tr>
-                        </table>
-                        <br/>
-                    </button>s
-                </form>
+                <?php
+                    for($i = 1;$i <= 3;$i++){
+                        $database->get_table($i);
+                        if(isset($_SESSION["table_id"])){
+                            if($_SESSION["table_id"] != 0){
+                                echo"
+                                    <form class = 'table'>
+                                        <br/>
+                                        <button type='submit' class = 'table-button'>
+                                            <br/>
+                                            <table width = '500px' hight = '300px' cellpading = '800' class = 'table-table'>
+                                                <tr>
+                                                    <td style ='color:#0066CC'>桌號:".$_SESSION["table_id"]."</td>
+                                                </tr>
+                                                <tr>
+                                                    <td rowspan = '2' style = 'color:#0066CC'>".$_SESSION["time"]."</td>
+                                                    <td style = 'color:#FFFFFF'>白方</td>
+                                                    <td>黑方</td>
+                                                    <td rowspan = '2' style = 'color:#0066CC'>遊玩中</td>
+                                                </tr>
+                                                <tr>
+                                                    <td style = 'color:#FFFFFF'><br/>".$_SESSION["player1"]."</td>
+                                                    <td><br/>".$_SESSION["player2"]."</td>
+                                                </tr>
+                                            </table>
+                                            <br/>
+                                        </button>
+                                    </form>";
+                            $_SESSION["table_id"] = 0;
+                            }
+                        }  
+                    }
+                    
+                ?>
             </div>
             <div class = "right">
                 <div class = "button">
-                    <form>
-                        <input type="submit" value="開設新房" class = "newtable-button">
+                    <form method="POST" action="lobby.php">
+                        <input type="submit" value="開設新房" class = "newtable-button" name="create">
                         <br/>
                         <br/>
-                        <input type="submit" value="上一頁" class = "other-button">
-                        <input type="submit" value="下一頁" class = "other-button">
-                        <input type="submit" value="刷新" class = "other-button">
-                        <input type="submit" value="離開" class = "other-button">
+                        <input type="button" value="上一頁" class = "other-button">
+                        <input type="button" value="下一頁" class = "other-button">
+                        <input type="button" value="刷新" class = "other-button" onclick="window.location.reload()"  >
+                        <input type="button" value="離開" class = "other-button" onclick="javascript:location.href='./index.php'">
                     </form>
                 </div>
             </div>
